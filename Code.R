f84cd98e28fd100260dd43f66d076b8347a8c19a
Michael Mackie <mjmackie94@gmail.com>
2022-03-05 12:18:54 +1100

    My test commit number 2

--- Code.R
+++ Code.R
@@ -6,3 +6,6 @@ library(tidyverse)
 # Commit:       (local) Staging to  (local) local repo
 # Push:         (local) Local   to  (cloud) Repo
 
+# Click STAGED to add to staging area, 
+#   recommented to add .gitignore and .rproj files too
+# use .gitignore to prevent files from being synced
