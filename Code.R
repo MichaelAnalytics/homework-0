40b48d7b122404c04760e10c6d9af6fee42c67db
Michael Mackie <mjmackie94@gmail.com>
2022-03-05 11:55:34 +1100

    Committing my first file with git

---
+++ Code.R
@@ -0,0 +1,8 @@
+# My code in the Homework-0 folder of my first git repository
+library(tidyverse)
+
+# Pull:         (cloud) Repo    to  (local) working directory
+# Add (stage):  (local) Working to  (local) staging area
+# Commit:       (local) Staging to  (local) local repo
+# Push:         (local) Local   to  (cloud) Repo
+
